ec2a08646772df33ac0b8a641bd4b148ecdc8d63
Landon Heaney <landonheaney@gmail.com>
2023-04-29 15:51:46 -0500

    inquirer questions

--- db/seeds.sql
+++ db/seeds.sql
@@ -15,20 +15,22 @@ VALUES (1, 'Engineer I', 60000.00, 1),
        (11, 'Sales III', 95000.00, 3),
        (12, 'Sales Manager', 115000.00, 3);
 
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (4, 'Jack', 'Doe', 4, NULL),
+       (9, 'Amelia', 'Smith', 8, NULL),
+       (14, 'Bella', 'Johnson', 12, NULL);
+
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id) 
 VALUES (1, 'John', 'Doe', 1, 4),
        (2, 'Jane', 'Doe', 2, 4),
        (3, 'Jim', 'Doe', 3, 4),
-       (4, 'Jack', 'Doe', 4, NULL),
        (5, 'Jill', 'Doe', 1, 4),
        (6, 'Alan', 'Smith', 5, 9),
        (7, 'Alice', 'Smith', 6, 9),
        (8, 'Alex', 'Smith', 7, 9),
-       (9, 'Amelia', 'Smith', 8, NULL),
        (10, 'Arnold', 'Smith', 5, 9),
        (11, 'Bob', 'Johnson', 9, 14),
        (12, 'Beth', 'Johnson', 10, 14),
        (13, 'Bill', 'Johnson', 11, 14),
-       (14, 'Bella', 'Johnson', 12, NULL),
        (15, 'Ben', 'Johnson', 9, 14);
 
--- index.js
+++ index.js
@@ -8,3 +8,42 @@ const connection = mysql.createConnection({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE
   });
+
+  const questions = [
+    {
+      type: 'list',
+      name: 'action',
+      message: 'What would you like to do?',
+      choices: [
+        'View all employees',
+        'Add employee',
+        'Update employee role',
+        'View all roles',
+        'Add role',
+        'View all departments',
+        'Add department',
+        'Quit'
+      ]
+    }
+  ];
+
+  // connection.query(
+  //   'SELECT * FROM employee WHERE manager_id = 14',
+  //   function(err, results, fields) {
+  //     console.log(results); // results contains rows returned by server
+  //   }
+  // );
+  // inquirer
+  // .prompt([
+  //   What
+  // ])
+  // .then((answers) => {
+  //   // Use user feedback for... whatever!!
+  // })
+  // .catch((error) => {
+  //   if (error.isTtyError) {
+  //     // Prompt couldn't be rendered in the current environment
+  //   } else {
+  //     // Something else went wrong
+  //   }
+  // });
